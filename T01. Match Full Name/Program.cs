737d00a66ffbdd700a3f71c502dcc34718e22ef8
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-03-17 21:56:22 +0200

    Add project files.

---
+++ T01. Match Full Name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace T01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]{1,} [A-Z][a-z]{2,}\b";
+
+            string names = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(names, pattern);
+
+            foreach (Match validName in matches)
+            {
+                Console.Write($"{validName.Value} ");
+            }
+        }
+    }
+}
---
+++ T02. Match Phone Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace T02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string validPhoneNumberPattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+            string phoneNums = Console.ReadLine();
+            
+            MatchCollection matches = Regex.Matches(phoneNums, validPhoneNumberPattern);
+
+            List<string> result = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                result.Add(match.ToString());
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
